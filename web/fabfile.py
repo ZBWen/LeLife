8517bafb1aed72327dd776d507e354f11706263e
ZBwen <qq360892272@126.com>
2017-09-27 22:12:07 +0800

    add web

---
+++ web/fabfile.py
@@ -0,0 +1,96 @@
+from fabric.api import *
+from fabric.contrib.files import *
+import time
+
+#deploy script for QCLOUD
+def production():
+    env.hosts=['47.52.91.196']
+    env.dbhost='127.0.0.1'
+    env.dbname='leLife'
+    env.dbuser='root'
+    env.dbpass='root'
+    env.dbport='3306'
+    env.dj_setting='production'
+    env.DB_migrate = True
+
+def tcdeployweb():
+    #deploy web
+    with cd("/mnt/github/leLife"):
+        run("git pull origin master")
+        run("pip3 install -r requirements.txt")
+        # run("supervisorctl stop ")
+    with settings(warn_only=True):          
+        run("sudo pkill uwsgi")        
+    with cd("/mnt/deploy_bak"):
+        run("rm -rf portal")
+        run("cp -r /mnt/deploy/portal portal")
+    with cd("/mnt/deploy"):
+        run("rm -rf portal")
+    with cd("/mnt/deploy/portal"):
+        if env.DB_migrate:
+            run("python manage.py migrate --noinput --settings=visa.settings.%s" % env.dj_setting)
+            run("python manage.py clearsessions --settings=visa.settings.%s" % env.dj_setting)
+        run("python manage.py collectstatic --noinput --settings=visa.settings.%s" % env.dj_setting)
+        # run("sudo supervisorctl start 51visa_uwsgi")
+
+
+def tcbackupdb_dl():
+    #db backup
+    require('dbhost')
+    require('dbname')
+    require('dbuser')
+    require('dbpass')
+    require('dbport')
+    require('dj_setting')
+
+    date = time.strftime('%Y%m%d%H%M%S')
+    fname = '/tmp/b51visa-%(database)s-backup-%(date)s.gz' % {
+        'database': env.dbname,
+        'date': date,
+    }
+
+    if exists(fname):
+        run('rm "%s"' % fname)
+
+    run('mysqldump --default-character-set=utf8mb4 -P %(dbport)s -h %(dbhost)s -u %(username)s -p%(password)s %(database)s  | '
+        'gzip > %(fname)s' % {'dbport': env.dbport,
+                              'dbhost': env.dbhost,
+                              'username': env.dbuser,
+                              'password': env.dbpass,
+                              'database': env.dbname,
+                              'fname': fname})
+
+    get(fname, os.path.basename(fname))
+    run('rm "%s"' % fname)
+
+
+def tcbackupdb_nodl():
+    #db backup
+    require('dbhost')
+    require('dbname')
+    require('dbuser')
+    require('dbpass')
+    require('dbport')
+    require('dj_setting')
+
+    date = time.strftime('%Y%m%d%H%M%S')
+    fname = '/tmp/b51visa-%(database)s-backup-%(date)s.gz' % {
+        'database': env.dbname,
+        'date': date,
+    }
+
+    if exists(fname):
+        run('rm "%s"' % fname)
+
+    run('mysqldump --default-character-set=utf8mb4 -P %(dbport)s -h %(dbhost)s -u %(username)s -p%(password)s %(database)s  | '
+        'gzip > %(fname)s' % {'dbport': env.dbport,
+                              'dbhost': env.dbhost,
+                              'username': env.dbuser,
+                              'password': env.dbpass,
+                              'database': env.dbname,
+                              'fname': fname})
+
+    #keep file on server, do not get backup
+    #get(fname, os.path.basename(fname))
+    #run('rm "%s"' % fname)
+
---
+++ web/web/settings/production.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leLife',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {'charset':'utf8mb4'},
+        'CONN_MAX_AGE': 600,
+    }
+}
+
+# CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#        'LOCATION': '54ac2433ff9b465e.m.cnhzaliqshpub001.ocs.aliyuncs.com:11211',
+#        'TIMEOUT': 3600*48,
+#        'KEY_PREFIX': 'leLife_'
+#    }
+# }
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ('rest_framework.renderers.JSONRenderer',)
