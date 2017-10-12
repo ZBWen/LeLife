68210161889899cd634054761b23cff03302b5f1
ZBwen <qq360892272@126.com>
2017-10-12 21:50:24 +0800

    edit

--- tasks/conf/production.py
+++ tasks/conf/production.py
@@ -43,7 +43,7 @@ CELERYBEAT_SCHEDULE = {
     'select-history-jbK8':{
         'task': 'tasks.service.lottery.Test',
         # 'schedule':crontab(minute='*/1'),
-        'schedule': crontab(hour=21,minute=36),
+        'schedule': crontab(hour=21,minute=51),
         "options":{},
         'args': ()
     },
