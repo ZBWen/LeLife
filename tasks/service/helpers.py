9b5551daf6be6067239a3c8e9ca149d9ce6e05b7
ZBwen <qq360892272@126.com>
2017-10-17 20:37:15 +0800

    edit code

--- tasks/service/helpers.py
+++ tasks/service/helpers.py
@@ -4,16 +4,17 @@ import datetime
 
 class IsRunTime(object):
 
-	def __init__(self, _str):
-		self._str = _str
-		self.now = datetime.datetime.now()
+    def __init__(self, _str):
+        self._str = _str
+        self.now = datetime.datetime.now()
 
-	def verify(self):
-		if self._str == 'NewPrevkeno':
-			self.prevkeno()
+    def verify(self):
+        if self._str == 'NewPrevkeno':
+            self.prevkeno()
 
-	def prevkeno(self):
-		if self.now.hour == 9:
-			if self.now.minute < 5:
-				return True
-		return False
+    def prevkeno(self):
+        if self.now.hour < 9:
+            return False
+        if self.now.hour == 9 and self.now.minute < 5:
+            return False
+        return True
