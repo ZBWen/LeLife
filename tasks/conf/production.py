7014c44c9bdf35dcfcab4efdb861f86f0ccccf8a
ZBwen <qq360892272@126.com>
2017-10-19 22:12:55 +0800

    edit

--- tasks/conf/production.py
+++ tasks/conf/production.py
@@ -36,7 +36,7 @@ CELERYBEAT_SCHEDULE = {
         'task': 'tasks.service.lottery.PrevkenoMiss',
         'schedule': crontab(
             hour='1,10,11,12,13,14,15,16,17,18,19,20,21,22,23',
-            minute='16'),
+            minute='3,8,13,18,23,28,33,38,43,48,53,58'),
         "options":{},
         'args': ()
     },
