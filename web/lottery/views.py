78b2d01226aeabb19e5d184bd93e70ecd0d04958
ZBwen <qq360892272@126.com>
2017-10-31 21:27:12 +0800

    end

--- web/lottery/views.py
+++ web/lottery/views.py
@@ -8,10 +8,11 @@ class ShowKenoView(TemplateView):
     template_name = 'keno/keno.html'
 
     def get(self, request, *args, **kwargs):
-      
+        kenos = BJkeNo.objects.all().order_by('-issue')
+        self.kenos = kenos[:50]
         return super(ShowKenoView,self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context_data = super(ShowKenoView,self).get_context_data(**kwargs)
-       
+        context_data['kenos'] = self.kenos
         return context_data
